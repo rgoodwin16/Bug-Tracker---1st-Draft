d8e51a4b7c398c540de796923fd20c2f56a3a61b
Ray Goodwin <rgoodwin16@outlook.com>
2015-07-27 19:44:09 -0400

    7/27/15
    Honestly...just started with this so this shouldn't really be anything
    important yet.

---
+++ BugTracker/BugTracker/Models/Tickets.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models
+{
+    public class Tickets
+    {
+        public int id { get; set; }
+        public string Title {get; set;}
+        public string Description {get; set;}
+        public System.DateTimeOffset Created { get; set; }
+        public Nullable<System.DateTimeOffset> Updated { get; set; }
+
+
+    }
+}
